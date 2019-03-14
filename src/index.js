77b4ff97648feb6a27824185b799a77efc428dbf
Phoebe LaBlonde <pmlablonde2220@gmail.com>
2019-03-14 13:36:42 -0700

    display job list based off API fetch and search term

--- src/index.js
+++ src/index.js
@@ -7,13 +7,13 @@ window.addEventListener('hashchange', () => {
     const hash = window.location.hash.slice(1);
     const searchOptions = readFromHash(hash);
     const searchUrl = makeSearchURL(searchOptions);
-    console.log(searchUrl);
-
     fetch(searchUrl)
         .then(response => response.json())
         .then(jobList => {
+            if(jobList.length === 0){
+                alert('NO SEARCH RESULTS FOUND');
+            }
             loadJobList(jobList);
-            console.log(jobList);
         });
 
 
--- src/makeSearchUrl.js
+++ src/makeSearchUrl.js
@@ -1,5 +1,5 @@
 export default function makeSearchUrl(searchOptions) {
-    const apiUrl = 'https://jobs.github.com/positions.json?';
+    const apiUrl = 'https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json?';
     const searchUrl = new URL(apiUrl);
     searchUrl.searchParams.set('location', searchOptions.location);
     searchUrl.searchParams.set('description', searchOptions.description);
