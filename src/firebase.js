68b5932db3eda2ad9d5eac00e9f57ef498d56529
Phoebe LaBlonde <pmlablonde2220@gmail.com>
2019-03-15 11:56:19 -0700

    fuckin

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,10 @@
 var config = {
-    apiKey: "AIzaSyBo8SHeQyewr7SnBoedcdsvchY8Vxv9igQ",
-    authDomain: "weather-app-5cb10.firebaseapp.com",
-    databaseURL: "https://weather-app-5cb10.firebaseio.com",
-    projectId: "weather-app-5cb10",
+    apiKey: 'AIzaSyBo8SHeQyewr7SnBoedcdsvchY8Vxv9igQ',
+    authDomain: 'weather-app-5cb10.firebaseapp.com',
+    databaseURL: 'https://weather-app-5cb10.firebaseio.com',
+    projectId: 'weather-app-5cb10',
 };
-firebase.initializeApp(config);
+export const app = firebase.initializeApp(config);
 
 export const auth = firebase.auth();
 export const db = firebase.database();
--- test/tests.js
+++ test/tests.js
@@ -2,3 +2,12 @@ import './html-equal.js';
 import './job-list-template.test.js';
 import './manipulate-hash.test.js';
 import './headerTemplate.test.js';
+// add this import from our firebase.js module:
+import { app } from '../src/firebase.js';
+import './html-equal.js';
+
+
+// add this line that cleans up firebase when test suite is done:
+QUnit.done(() => {
+    app.delete();
+});
